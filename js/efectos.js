281270a4ea4081d52a7b6094978623f9c33a4990
Marcial Reyes <marcial.kings@gmail.com>
2018-09-26 11:07:52 -0600

    pagina terminada

---
+++ js/efectos.js
@@ -0,0 +1,104 @@
+/*efecto al colocar puntero sobre menu */ 
+/*al colocar el puntero */
+$("#inicio").mouseover(function(){   
+    $("#inicio").height("40px");
+    $("#inicio").width("140px");    
+});
+
+$("#galeria").mouseover(function(){   
+    $("#galeria").height("40px");
+    $("#galeria").width("140px");   
+});
+$("#ubicacion").mouseover(function(){   
+    $("#ubicacion").height("40px");
+    $("#ubicacion").width("140px");   
+});
+
+$("#links").mouseover(function(){   
+    $("#links").height("40px");
+    $("#links").width("140px");   
+});
+
+$("#contacto").mouseover(function(){   
+    $("#contacto").height("40px");
+    $("#contacto").width("140px");   
+});
+
+/*al quitar el puntero */
+$("#inicio").mouseout(function(){    
+    $("#inicio").height("27px");
+    $("#inicio").width("135px");;  
+});
+
+$("#galeria").mouseout(function(){    
+    $("#galeria").height("27px");
+    $("#galeria").width("135px"); 
+});
+
+$("#ubicacion").mouseout(function(){    
+    $("#ubicacion").height("27px");
+    $("#ubicacion").width("135px");  
+});
+
+$("#links").mouseout(function(){    
+    $("#links").height("27px");
+    $("#links").width("135px");
+});
+
+$("#contacto").mouseout(function(){    
+    $("#contacto").height("27px");
+    $("#contacto").width("135px"); 
+});
+
+/* -------------------iconos----------------------- */
+/* al colocar el puntero sobre*/
+$("#google").mouseover(function(){   
+    $("#google").height("50px");
+    $("#google").width("50px");    
+});
+
+$("#twitter").mouseover(function(){   
+    $("#twitter").height("50px");
+    $("#twitter").width("50px");    
+});
+
+$("#youtube").mouseover(function(){   
+    $("#youtube").height("50px");
+    $("#youtube").width("50px");    
+});
+
+$("#instagram").mouseover(function(){   
+    $("#instagram").height("50px");
+    $("#instagram").width("50px");  
+});
+
+$("#facebook").mouseover(function(){   
+    $("#facebook").height("50px");
+    $("#facebook").width("50px");    
+});
+
+/*al quitar puntero*/
+$("#facebook").mouseout(function(){    
+    $("#facebook").height("35px");
+    $("#facebook").width("35px");    
+});
+
+$("#instagram").mouseout(function(){    
+    $("#instagram").height("35px");
+    $("#instagram").width("35px");    
+});
+
+$("#youtube").mouseout(function(){    
+    $("#youtube").height("35px");
+    $("#youtube").width("35px");    
+});
+
+$("#twitter").mouseout(function(){    
+    $("#twitter").height("35px");
+    $("#twitter").width("35px");    
+});
+
+$("#google").mouseout(function(){    
+    $("#google").height("35px");
+    $("#google").width("35px");    
+});
